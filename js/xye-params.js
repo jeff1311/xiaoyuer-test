c680166729b8124ef6ad0d94e326189a58603009
ljf <285079883@qq.com>
2020-02-27 18:12:43 +0800

    style

---
+++ js/xye-params.js
@@ -0,0 +1,9 @@
+// var baseUrl = "https://www.yu.com";
+var baseUrl = "https://www1.xiaoyuer.net";
+
+// var xyeAct = "18761637019_36";
+// var openNo = "021701320";
+// var secret = "62B2F6D457243FBD49B5E39C5D6E41A3";
+var xyeAct = "18761637019_1";
+var openNo = "021809498";
+var secret = "934FEE184EF0CD76505E02F14DC45606";
